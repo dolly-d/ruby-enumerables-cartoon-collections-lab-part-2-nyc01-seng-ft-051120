dccad15c1b6ed956a047a56c1cb57037ef4c5fb2
Dolly Desir <dolly.desir@gmail.com>
2020-05-07 18:38:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ end
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
-  while long_planeteer_calls
+  while long_planeteer_calls do
     if 
   
   end
