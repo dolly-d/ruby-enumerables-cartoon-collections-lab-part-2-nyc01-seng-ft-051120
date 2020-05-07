ceca7ade4901b7bb5877c968af2e99f167bb73b5
Dolly Desir <dolly.desir@gmail.com>
2020-05-07 19:08:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -35,5 +35,7 @@ def find_valid_calls(planeteer_calls)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
   i = 0 
-  
+  while i < find_valid_calls do
+    if find_valid_calls.find 
+  end
 end
