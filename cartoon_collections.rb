d0d967fca25e1b8ef2271f33dd34d924d6e53542
Dolly Desir <dolly.desir@gmail.com>
2020-05-07 18:32:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def summon_captain_planet(planeteer_calls)
   # Return a new array of the results
   new_array = []
   planeteer_calls.each do |call|
-    new_array << ""
+    new_array << "#{call}"
   end 
 end
 
